e1f9d709c49f14699fc3492a581d91edb7ef4e02
sebhuro <sebhuro@gmail.com>
2022-01-09 11:13:51 -0500

    modificaciones al model y urls del proyecto

--- gestionventas/models.py
+++ gestionventas/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class marcasvehiculos(models.Model):
+    marca=models.CharField(max_length=25)
+    serie=models.CharField(max_length=25)
+    ano_fabricacion=models.IntegerField(max_length=4)
+    pais_fabricacion=models.CharField(max_length=25)
+    Precio=models.IntegerField()
+
+class vendedor(models.Model):
+    nombre_vendedor=models.CharField(max_length=35)
+    apellido_vendedor=models.CharField(max_length=35)
+    email_vendedor=models.EmailField(max_length=30)
+    sucursal=models.CharField(max_length=30)
--- luxurycars/settings.py
+++ luxurycars/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gestionventas',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'luxurycars.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["C:/Prodev/proyectofinalcoder/luxurycars/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
